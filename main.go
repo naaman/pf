9e0c344267a24781109032fc8527cc09e6a34b8b
Naaman Newbold <naaman@heroku.com>
2013-12-25 13:10:55 -0800

    Simple procfile parser

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "os"
+  "flag"
+  "fmt"
+)
+
+var procfileFlag = flag.String("procfile", "", "Procfile path")
+
+func main() {
+  flag.Parse()
+
+  procfileFile, err := os.Open(*procfileFlag)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  
+  procfile, err := ParseProcfile(procfileFile)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+
+  for _, e := range procfile.Entries {
+    fmt.Printf("%s:%s\n", e.Type, e.Command)
+  }
+}
